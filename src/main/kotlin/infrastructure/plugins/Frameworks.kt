6f008eabb273a32f7111cc1b190c4fb87d1ca140
vluce <vluce@ippon.fr>
2025-03-27 17:21:27 +0100

    feat: update config and downgrade to ktor 2.3.13 for koin

--- src/main/kotlin/infrastructure/plugins/Frameworks.kt
+++ src/main/kotlin/infrastructure/plugins/Frameworks.kt
@@ -1,6 +1,12 @@
 package com.ippon.vluce.infrastructure.plugins
 
+import com.ippon.vluce.application.playerModule
 import io.ktor.server.application.*
+import org.koin.ktor.plugin.Koin
+import org.koin.logger.slf4jLogger
 
 fun Application.configureFrameworks() {
+    install(Koin) {
+        slf4jLogger()
+    }
 }
